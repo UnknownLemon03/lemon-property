8513554b4a4c008b5d02b615cc808fee5fa9adbf
UnknownLemon03 <lemon.sunomono03@gmail.com>
2025-05-31 12:31:59 +0530

    added a url

--- backend-api/src/index.ts
+++ backend-api/src/index.ts
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"],
     preflightContinue: false,
